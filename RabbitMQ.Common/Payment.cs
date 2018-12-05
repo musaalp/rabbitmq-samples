aa1ffc702d3f3cf12f56f1b3f33d015a37221590
Musa Alp <musaa.alp@gmail.com>
2018-12-05 11:35:12 +0300

    add common layer

---
+++ RabbitMQ.Common/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.Common
+{
+    [Serializable]
+    public class Payment
+    {
+        public decimal Amount { get; set; }
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RabbitMQ.Common/PurchaseOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RabbitMQ.Common
+{
+    [Serializable]
+    public class PurchaseOrder
+    {
+        public decimal Amount { get; set; }
+        public string PurchaseOrderNumber { get; set; }
+        public string CompanyName { get; set; }
+        public int PaymentDayTerms { get; set; }
+    }
+}
