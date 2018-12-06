f098f8e3c791228c48151e9057d79f0f45054d25
Musa Alp <musaa.alp@gmail.com>
2018-12-06 11:54:16 +0300

    add worker queue sample

---
+++ RabbitMQ.WorkerQueue.Consumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Common;
+using System;
+
+namespace RabbitMQ.WorkerQueue.Consumer
+{
+    class Program
+    {
+        private static ConnectionFactory _connectionFactory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string QueueName = "WorkerQueue_Queue";
+
+        static void Main(string[] args)
+        {
+            CreateQueue();
+
+            Receive();
+        }
+
+        private static void CreateQueue()
+        {
+            _connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _connectionFactory.CreateConnection();
+
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+            _model.BasicQos(0, 1, false);
+        }
+
+        public static void Receive()
+        {
+            var consumer = new QueueingBasicConsumer(_model);
+            _model.BasicConsume(QueueName, false, consumer);
+
+            while (true)
+            {
+                var ea = consumer.Queue.Dequeue();
+                var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+                _model.BasicAck(ea.DeliveryTag, false);
+
+                Console.WriteLine($" Payment message received : {message.CardNumber} | {message.Amount} | {message.Name}");
+            }
+        }
+
+        //private static void Receive()
+        //{
+        //    var consumer = new EventingBasicConsumer(_model);
+
+        //    var strMessage = _model.BasicConsume(QueueName, true, consumer);
+
+        //    consumer.Received += Consumer_Received;
+        //}
+
+        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    //todo: after receive second message the model is closing
+        //    var message = e.Body.DeSerialize(typeof(Payment)) as Payment;
+        //    _model.BasicAck(e.DeliveryTag, false);
+
+        //    Console.WriteLine($" Message Received : {message.CardNumber} | {message.Amount} | {message.Name}");
+        //}
+    }
+}
---
+++ RabbitMQ.WorkerQueue.Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Common;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RabbitMQ.WorkerQueue.Producer
+{
+    class Program
+    {
+        private const string QueueName = "WorkerQueue_Queue";
+
+        private static ConnectionFactory _connectionFactory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        static void Main(string[] args)
+        {
+            var payments = CreatePayments(5154);
+
+            CreateQueue();
+
+            foreach (var payment in payments)
+            {
+                Thread.Sleep(1000);
+                SendMessage(payment);
+            }
+        }
+
+        private static IEnumerable<Payment> CreatePayments(int sampleCount)
+        {
+            var paymentList = new List<Payment>();
+
+            for (int i = 1; i < sampleCount; i++)
+            {
+                var payment = new Payment
+                {
+                    Amount = new Random().Next(1, 10) * i,
+                    CardNumber = i.ToString(),
+                    Name = $"Customer Name {i}"
+                };
+
+                paymentList.Add(payment);
+            }
+
+            return paymentList;
+        }
+
+        private static void CreateQueue()
+        {
+            _connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _connectionFactory.CreateConnection();
+
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static void SendMessage(Payment message)
+        {
+            _model.BasicPublish("", QueueName, null, message.Serialize());
+            Console.WriteLine($"Payment message sent : {message.CardNumber} | {message.Amount} | {message.Name}");
+        }
+    }
+}
