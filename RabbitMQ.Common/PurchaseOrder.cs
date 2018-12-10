89cda7ecfb1bec528e03dd07150317454f320794
Musa Alp <musaa.alp@gmail.com>
2018-12-10 17:14:42 +0300

    change a prop type of PurchaseOrder

--- RabbitMQ.Common/PurchaseOrder.cs
+++ RabbitMQ.Common/PurchaseOrder.cs
@@ -6,7 +6,7 @@ namespace RabbitMQ.Common
     public class PurchaseOrder
     {
         public decimal Amount { get; set; }
-        public string PurchaseOrderNumber { get; set; }
+        public int PurchaseOrderNumber { get; set; }
         public string CompanyName { get; set; }
         public int PaymentDayTerms { get; set; }
     }
