a78bde9a0a68b93857ae3b41c7032a5b32349328
Musa Alp <musaa.alp@gmail.com>
2018-12-10 17:15:04 +0300

    add empty consumers of sample app

---
+++ SampleApp.AccountAudit.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApp.AccountAudit.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SampleApp.PaymentCard.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApp.PaymentCard.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
