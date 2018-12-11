089221ed7e9dc2c108cc510994013fd222859496
Musa Alp <musaa.alp@gmail.com>
2018-12-11 16:04:25 +0300

    add web api project

---
+++ SampleApp.WebAPI/RabbitMQ/RabbitMQClient.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Common;
+using SampleApp.WebAPI.Models;
+using System;
+
+namespace SampleApp.WebAPI.RabbitMQ
+{
+    public class RabbitMQClient
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string ExchangeName = "Topic_Exchange";
+        private const string CardPaymentQueueName = "CardPaymentTopic_Queue";
+        private const string PurchaseOrderQueueName = "PurchaseOrderTopic_Queue";
+        private const string AllQueueName = "AllTopic_Queue";
+
+        public RabbitMQClient()
+        {
+            CreateConnection();
+        }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.ExchangeDeclare(ExchangeName, "topic");
+
+            _model.QueueDeclare(CardPaymentQueueName, true, false, false, null);
+            _model.QueueDeclare(PurchaseOrderQueueName, true, false, false, null);
+            _model.QueueDeclare(AllQueueName, true, false, false, null);
+
+            _model.QueueBind(CardPaymentQueueName, ExchangeName, "payment.card");
+            _model.QueueBind(PurchaseOrderQueueName, ExchangeName, "payment.purchaseorder");
+            _model.QueueBind(AllQueueName, ExchangeName, "payment.*");
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+        public void SendPayment(CardPayment payment)
+        {
+            SendMessage(payment.Serialize(), "payment.card");
+            Console.WriteLine(" Payment Sent {0}, £{1}", payment.Number,
+                payment.Amount);
+        }
+
+        public void SendPurchaseOrder(Models.PurchaseOrder purchaseOrder)
+        {
+            SendMessage(purchaseOrder.Serialize(), "payment.purchaseorder");
+
+            Console.WriteLine(" Purchase Order Sent {0}, £{1}, {2}, {3}",
+                purchaseOrder.CompanyName, purchaseOrder.Amount,
+                purchaseOrder.PaymentDayTerms, purchaseOrder.Number);
+        }
+
+        private void SendMessage(byte[] message, string routingKey)
+        {
+            _model.BasicPublish(ExchangeName, routingKey, null, message);
+        }
+    }
+}
---
+++ SampleApp.WebAPI/RabbitMQ/RabbitMQDirectClient.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Common;
+using SampleApp.WebAPI.Models;
+using System;
+using System.Text;
+
+namespace SampleApp.WebAPI.RabbitMQ
+{
+    public class RabbitMQDirectClient
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private string _replyQueueName;
+        private string _authCode;
+        private string _corrId;
+        private EventingBasicConsumer consumer;
+
+        public void CreateConnection()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _replyQueueName = _channel.QueueDeclare("rpc_reply", true, false, false, null);
+
+            consumer = new EventingBasicConsumer(_channel);
+
+
+            //_consumer = new QueueingBasicConsumer(_channel);
+            _channel.BasicConsume(_replyQueueName, true, consumer);
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+        public string MakePayment(CardPayment payment)
+        {
+            _corrId = Guid.NewGuid().ToString();
+            var props = _channel.CreateBasicProperties();
+            props.ReplyTo = _replyQueueName;
+            props.CorrelationId = _corrId;
+
+            _channel.BasicPublish("", "rpc_queue", props, payment.Serialize());
+
+            consumer.Received += Consumer_Received;
+
+            while (true)
+            {
+                consumer.Received += Consumer_Received;
+
+                //var ea = _consumer.Queue.Dequeue();
+
+                //if (ea.BasicProperties.CorrelationId != corrId) continue;
+
+                //var authCode = Encoding.UTF8.GetString(ea.Body);
+                //return authCode;
+            }
+
+            return _authCode;
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            if (e.BasicProperties.CorrelationId == _corrId)
+            {
+                _authCode = Encoding.UTF8.GetString(e.Body);
+            }
+        }
+    }
+}
