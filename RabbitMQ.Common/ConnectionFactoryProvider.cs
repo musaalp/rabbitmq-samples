84af127becd9c9fe62cf9debb7ece832660a1b00
Musa Alp <musaa.alp@gmail.com>
2018-12-13 11:43:49 +0300

    split responsibilities refactore common project

---
+++ RabbitMQ.Common/ConnectionFactoryProvider.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQ.Common
+{
+    public class ConnectionFactoryProvider
+    {
+        public static ConnectionFactory Get()
+        {
+            return new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+        }
+    }
+}
---
+++ RabbitMQ.Common/SampleDataCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RabbitMQ.Common
+{
+    public class SampleDataCreator
+    {
+        public IEnumerable<Payment> CreatePayments(int sampleCount)
+        {
+            var paymentList = new List<Payment>();
+
+            for (int i = 1; i < sampleCount; i++)
+            {
+                var payment = new Payment
+                {
+                    Amount = new Random().Next(1, 10) * i,
+                    CardNumber = i.ToString(),
+                    Name = $"Customer Name {i}"
+                };
+
+                paymentList.Add(payment);
+            }
+
+            return paymentList;
+        }
+    }
+}
