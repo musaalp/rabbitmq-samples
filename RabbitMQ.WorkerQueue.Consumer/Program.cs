87f2b18a8f8548db0dd40a7e3a2b0eb5104137b4
Musa Alp <musaa.alp@gmail.com>
2018-12-14 11:00:44 +0300

    improve ui readability and naming

--- RabbitMQ.WorkerQueue.Consumer/Program.cs
+++ RabbitMQ.WorkerQueue.Consumer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RabbitMQ.Common;
 using System;
 
@@ -6,66 +7,44 @@ namespace RabbitMQ.WorkerQueue.Consumer
 {
     class Program
     {
-        private static ConnectionFactory _connectionFactory;
-        private static IConnection _connection;
-        private static IModel _model;
-
         private const string QueueName = "WorkerQueue_Queue";
 
         static void Main(string[] args)
         {
-            CreateQueue();
+            var connectionFactory = ConnectionFactoryProvider.Get();
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
 
-            Receive();
+            CreateQueue(channel);
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine(string.Format("|{0,26}|{1,15}|{2,15}|{3,20}|", "Description", "Card Number", "Amount", "Name"));
+            Console.WriteLine("---------------------------------------------------------------------------------");
+
+            ReceiveMessages(channel);
         }
 
-        private static void CreateQueue()
+        private static void CreateQueue(IModel channel)
         {
-            _connectionFactory = new ConnectionFactory
+            channel.QueueDeclare(QueueName, true, false, false, null);
+            //This tells RabbitMQ not to give more than one message to a worker at a time
+            channel.BasicQos(0, 1, false);
+        }
+
+        public static void ReceiveMessages(IModel channel)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (ch, eventArgs) =>
             {
-                HostName = "localhost",
-                UserName = "guest",
-                Password = "guest"
+                var message = (Payment)eventArgs.Body.DeSerialize(typeof(Payment));
+                channel.BasicAck(eventArgs.DeliveryTag, false);
+
+                Console.WriteLine("Payment message received : " + string.Format("|{0,15}|{1,15}|{2,20}|", message.CardNumber, message.Amount, message.Name));
             };
 
-            _connection = _connectionFactory.CreateConnection();
-
-            _model = _connection.CreateModel();
-            _model.QueueDeclare(QueueName, true, false, false, null);
-            _model.BasicQos(0, 1, false);
+            channel.BasicConsume(QueueName, false, consumer);
         }
-
-        public static void Receive()
-        {
-            var consumer = new QueueingBasicConsumer(_model);
-            _model.BasicConsume(QueueName, false, consumer);
-
-            while (true)
-            {
-                var ea = consumer.Queue.Dequeue();
-                var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
-                _model.BasicAck(ea.DeliveryTag, false);
-
-                Console.WriteLine($" Payment message received : {message.CardNumber} | {message.Amount} | {message.Name}");
-            }
-        }
-
-        //private static void Receive()
-        //{
-        //    var consumer = new EventingBasicConsumer(_model);
-
-        //    var strMessage = _model.BasicConsume(QueueName, true, consumer);
-
-        //    consumer.Received += Consumer_Received;
-        //}
-
-        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
-        //{
-        //    //todo: after receive second message the model is closing
-        //    var message = e.Body.DeSerialize(typeof(Payment)) as Payment;
-        //    _model.BasicAck(e.DeliveryTag, false);
-
-        //    Console.WriteLine($" Message Received : {message.CardNumber} | {message.Amount} | {message.Name}");
-        //}
     }
 }
