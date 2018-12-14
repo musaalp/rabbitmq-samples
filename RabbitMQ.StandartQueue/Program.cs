27d11dca15ed76bde4472361443643d0480cc6eb
Musa Alp <musaa.alp@gmail.com>
2018-12-14 10:56:32 +0300

    change way of using EventingBasicConsumer event

--- RabbitMQ.StandartQueue/Program.cs
+++ RabbitMQ.StandartQueue/Program.cs
@@ -23,6 +23,7 @@ namespace RabbitMQ.StandartQueue
             Console.WriteLine("-----------------------------------------------------------------------------");
             Console.WriteLine(string.Format("|{0,22}|{1,15}|{2,15}|{3,20}|", "Description", "Card Number", "Amount", "Name"));
             Console.WriteLine("-----------------------------------------------------------------------------");
+
             foreach (var payment in payments)
             {
                 SendMessage(payment, channel);
@@ -47,19 +48,19 @@ namespace RabbitMQ.StandartQueue
             Console.WriteLine("Payment message sent : " + string.Format("|{0,15}|{1,15}|{2,20}|", message.CardNumber, message.Amount, message.Name));
         }
 
-        private static void ReceiveMessages(IModel channel)
+        public static void ReceiveMessages(IModel channel)
         {
             var consumer = new EventingBasicConsumer(channel);
 
-            var strMessage = channel.BasicConsume(QueueName, true, consumer);
+            consumer.Received += (ch, eventArgs) =>
+            {
+                var message = (Payment)eventArgs.Body.DeSerialize(typeof(Payment));
+                channel.BasicAck(eventArgs.DeliveryTag, false);
 
-            consumer.Received += Consumer_Received;
-        }
+                Console.WriteLine("Payment message received : " + string.Format("|{0,15}|{1,15}|{2,20}|", message.CardNumber, message.Amount, message.Name));
+            };
 
-        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
-        {
-            var message = e.Body.DeSerialize(typeof(Payment)) as Payment;
-            Console.WriteLine("Payment message received : " + string.Format("|{0,15}|{1,15}|{2,20}|", message.CardNumber, message.Amount, message.Name));
+            channel.BasicConsume(QueueName, false, consumer);
         }
     }
 }
