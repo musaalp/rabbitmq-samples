98670b0866ec37fd2496d4bb6a2a1da2d52f73d2
Musa Alp <musaa.alp@gmail.com>
2018-12-25 16:33:19 +0300

    refactore remove unnecessary objects

--- RabbitMQ.PublishSubscribe.Subscriber/Program.cs
+++ RabbitMQ.PublishSubscribe.Subscriber/Program.cs
@@ -8,38 +8,37 @@ namespace RabbitMQ.PublishSubscribe.Subscriber
     {
         private const string ExchangeName = "PublishSubscribe_Exchange";
 
-        private static ConnectionFactory _factory;
-        private static IConnection _connection;
-        private static QueueingBasicConsumer _consumer;
-
         static void Main(string[] args)
         {
-            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
-            using (_connection = _factory.CreateConnection())
+            var connectionFactory = ConnectionFactoryProvider.Get();
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            var consumer = new QueueingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            DeclareAndBindQueueToExchange(channel, queueName);
+
+            channel.BasicConsume(queueName, true, consumer);
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine(string.Format("|{0,26}|{1,15}|{2,15}|{3,20}|", "Description", "Card Number", "Amount", "Name"));
+            Console.WriteLine("---------------------------------------------------------------------------------");
+
+            while (true)
             {
-                using (var channel = _connection.CreateModel())
-                {
-                    var queueName = DeclareAndBindQueueToExchange(channel);
-                    channel.BasicConsume(queueName, true, _consumer);
-
-                    while (true)
-                    {
-                        var ea = _consumer.Queue.Dequeue();
-                        var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
-
-                        Console.WriteLine("----- Payment Processed {0} : {1}", message.CardNumber, message.Amount);
-                    }
-                }
+                var ea = consumer.Queue.Dequeue();
+                var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+                
+                Console.WriteLine("Payment message received : " + string.Format("|{0,15}|{1,15}|{2,20}|", message.CardNumber, message.Amount, message.Name));
             }
         }
 
-        private static string DeclareAndBindQueueToExchange(IModel channel)
+        private static void DeclareAndBindQueueToExchange(IModel channel, string queueName)
         {
-            channel.ExchangeDeclare(ExchangeName, "fanout");
-            var queueName = channel.QueueDeclare().QueueName;
+            channel.ExchangeDeclare(ExchangeName, "fanout");            
             channel.QueueBind(queueName, ExchangeName, "");
-            _consumer = new QueueingBasicConsumer(channel);
-            return queueName;
         }
     }
 }
