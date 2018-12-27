910935905e8541b2e7ebb39e840588ab1a6eadca
Musa Alp <musaa.alp@gmail.com>
2018-12-27 17:33:08 +0300

    enhance data creator

--- RabbitMQ.Common/SampleDataCreator.cs
+++ RabbitMQ.Common/SampleDataCreator.cs
@@ -7,21 +7,49 @@ namespace RabbitMQ.Common
     {
         public IEnumerable<Payment> CreatePayments(int sampleCount)
         {
+            var customerList = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K' };
+
             var paymentList = new List<Payment>();
 
             for (int i = 1; i < sampleCount; i++)
             {
+                var randomNumber = new Random().Next(1, customerList.Length - 1);
+
                 var payment = new Payment
                 {
-                    Amount = new Random().Next(1, 10) * i,
-                    CardNumber = i.ToString(),
-                    Name = $"Customer Name {i}"
+                    Name = customerList[randomNumber].ToString(),
+                    Amount = randomNumber * 7.3M,
+                    CardNumber = i.ToString()
                 };
 
                 paymentList.Add(payment);
             }
 
             return paymentList;
+        }
+
+        public IEnumerable<PurchaseOrder> CreatePurchaseOrder(int sampleCount)
+        {
+            var companyList = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K' };
+
+            var purchaseOrderList = new List<PurchaseOrder>();
+
+            for (int i = 1; i < sampleCount; i++)
+            {
+                var randomNumber = new Random().Next(1, companyList.Length - 1);
+
+                var payment = new PurchaseOrder
+                {
+                    CompanyName = companyList[randomNumber].ToString(),
+                    Amount = randomNumber * 12.7M,
+                    PaymentDayTerms = randomNumber * 5,
+                    PurchaseOrderNumber = i
+                };
+
+                purchaseOrderList.Add(payment);
+            }
+
+            return purchaseOrderList;
         }
     }
 }
